b4c4a72faaa9838e70fa5252923012d9d15f0baa
Ian Leith <ian@sigilion.com>
2014-03-27 16:46:26 +0000

    Hide get involved panel if empty.

--- drama/contexts.py
+++ drama/contexts.py
@@ -8,9 +8,29 @@ def venue(self, **kwargs):
     venue = context['object']
     context['shows'] = Show.objects.filter(performance__venue=venue).distinct()
     context['auditions'] = AuditionInstance.objects.filter(audition__show__performance__venue=venue).filter(end_datetime__gte=timezone.now()).order_by('end_datetime','start_time').distinct()
+    try:
+        context['auditions'][0]
+    except IndexError:
+        context['auditions'] = None
+        
     context['techieads'] = TechieAd.objects.filter(show__performance__venue=venue).filter(deadline__gte=timezone.now()).order_by('deadline').distinct()
+    try:
+        context['techieads'][0]
+    except IndexError:
+        context['techieads'] = None
+        
     context['showapps'] = ShowApplication.objects.filter(show__performance__venue=venue).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['showapps'][0]
+    except IndexError:
+        context['showapps'] = None
+        
     context['venueapps'] = VenueApplication.objects.filter(venue=venue).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['venueapps'][0]
+    except IndexError:
+        context['venueapps'] = None
+        
     context['current_pagetype']='venues'
     return context
 
@@ -19,9 +39,29 @@ def society(self, **kwargs):
     society = context['object']
     context['shows'] = society.show_set
     context['auditions'] = AuditionInstance.objects.filter(audition__show__society=society).filter(end_datetime__gte=timezone.now()).order_by('end_datetime','start_time')
+    try:
+        context['auditions'][0]
+    except IndexError:
+        context['auditions'] = None
+        
     context['techieads'] = TechieAd.objects.filter(show__society=society).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['techieads'][0]
+    except IndexError:
+        context['techieads'] = None
+        
     context['showapps'] = ShowApplication.objects.filter(show__society=society).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['showapps'][0]
+    except IndexError:
+        context['showapps'] = None
+        
     context['societyapps'] = SocietyApplication.objects.filter(society=society).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['societyapps'][0]
+    except IndexError:
+        context['societyapps'] = None
+        
     context['current_pagetype']='societies'
     return context
 
@@ -34,7 +74,17 @@ def show(self, **kwargs):
     context['prod'] = company.filter(role__cat='prod')
     context['performances'] = show.performance_set.all()
     context['auditions'] = AuditionInstance.objects.filter(audition__show=show).filter(end_datetime__gte=timezone.now()).order_by('end_datetime','start_time')
+    try:
+        context['auditions'][0]
+    except IndexError:
+        context['auditions'] = None
+        
     context['applications'] = ShowApplication.objects.filter(show=show).filter(deadline__gte=timezone.now()).order_by('deadline')
+    try:
+        context['applications'][0]
+    except IndexError:
+        context['applications'] = None
+        
     try:
         context['techiead'] = TechieAd.objects.filter(show=show).get()
     except TechieAd.DoesNotExist:
