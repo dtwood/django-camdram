a91aad747d38423d59f1765f4ee3ed3f0a05e55f
Ian Leith <ian@sigilion.com>
2014-05-31 19:14:56 +0100

    Update for Django 1.7

---
+++ drama/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class DramaConfig(AppConfig):
+    name = 'drama'
+    verbose_name = 'Camdram main app'
+    def ready(self):
+        import drama.signals
+        
---
+++ drama/signals.py
@@ -0,0 +1,17 @@
+from django.dispatch import receiver
+from registration import signals
+from drama import models
+from guardian.shortcuts import assign_perm
+
+
+@receiver(signals.user_activated)
+def pending_admins(sender, **kwargs):
+    email = kwargs['user'].email
+    user = kwargs['user']
+    for pa in models.PendingAdmin.objects.filter(email=email):
+        assign_perm('drama.change_show', user, pa.show)
+        pa.delete()
+    for pg in models.PendingGroupMember.objects.filter(email=email):
+        pg.group.user_set.add(user)
+        pg.delete()
+        
