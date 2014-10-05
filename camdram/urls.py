000e4f4d28a2a0f3dd2fc0cf76d0cf1a94a53aae
Ian Leith <ian@sigilion.com>
2014-10-05 20:36:07 +0100

    Add Lightbox, resolves #32

--- camdram/urls.py
+++ camdram/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'issues/', include('issues.urls', namespace='issues', app_name='issues')),
     url(r'', include('drama.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
             
